cb77c4b246f0117e18c9894d51480b2c69a7b25e
mishaaushev <aushev97@gmail.com>
2023-02-20 00:23:57 +0100

    created command console

--- MyVidGenExec.py
+++ MyVidGenExec.py
@@ -1,30 +1,65 @@
-from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
+import os
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QPlainTextEdit, QHBoxLayout, QVBoxLayout
 
+class MyApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        #Window Size
+        self.window_width, self.window_height = 720, 900
+        self.setMinimumSize(self.window_width, self.window_height)
 
-def my_ui_function():
-    app = QApplication(sys.argv)
-    win = QMainWindow()
-    win.setGeometry(200, 200, 300, 300)
-    win.setWindowTitle("Music Video Generator")
-    
-    label = QtWidgets.QLabel(win)
-    label.setText("Music Video Generator")
-    label.move(50,50)
-    
-    b1 = QtWidgets.QPushButton(win)
-    b1.setText("Click me")
-    b1.clicked.connect(clicked)
-    
-    win.show()
-    sys.exit(app.exec_())
+        #Window Title
+        self.setWindowTitle('MVGEN')
+        layout = QVBoxLayout()
+        self.setLayout(layout)
 
-def clicked():
-    
-    # Import the module
-    import MyVidGen
+        #Command Line input Widget
+        #self.editorCommand = QPlainTextEdit()
+        #layout.addWidget(self.editorCommand, 3)
 
+        #Command Line Output Widget
+        self.editorOutput = QPlainTextEdit()
+        layout.addWidget(self.editorOutput, 7)
+
+        buttonLayout = QHBoxLayout()
+        layout.addLayout(buttonLayout)
+
+        #Main Button
+        self.button_run = QPushButton('&Run Command', clicked=self.runCommand)
+        buttonLayout.addWidget(self.button_run)
+
+        #Clear Button
+        self.button_clear = QPushButton('&Clear', clicked=lambda: self.editorOutput.clear())
+        buttonLayout.addWidget(self.button_clear)
+
+        #self.editorCommand.insertPlainText('dir')
+
+    def runCommand(self):
+        import MyVidGen
         
-    #Call the function from MyVidGen.py
-    MyVidGen.my_vid_gen_exec()    
+        #Call the function from MyVidGen.py
+        MyVidGen.my_vid_gen_exec()   
+        
+
+if __name__ == '__main__':
+    # don't auto scale when drag app to a different monitor.
+    # QApplication.setAttribute(Qt.HighDpiScaleFactorRoundingPolicy.PassThrough)
+    
+    #App Instance
+    app = QApplication(sys.argv)
+    #App Style
+    app.setStyleSheet('''
+        QWidget {
+            font-size: 30px;
+        }
+    ''')
+    
+    myApp = MyApp()
+    myApp.show()
+
+    try:
+        sys.exit(app.exec_())
+    except SystemExit:
+        print('Closing Window...')
+
