7afc6c722bc91121ed2bd70042142ef9dd2d4afc
Mykhaylo <aushev97@gmail.com>
2023-07-04 00:14:27 +0200

    updated the MyVidGen logic

--- MyVidGenOne.py
+++ MyVidGenOne.py
@@ -1,33 +1,30 @@
 def my_vid_gen_exec():
     import json
-    import random
     import requests
     import librosa
     import soundfile as sf
     import pydub
     import wave
     from tqdm import tqdm
-
     from moviepy.editor import VideoFileClip, concatenate_videoclips, AudioFileClip
+    import urllib.request
 
     print("Starting VidGenerator...")
 
-    # Fetch multiple random videos from Pexels API
+    # Fetch a random video from Pexels API
     response = requests.get("https://api.pexels.com/videos/search?query=beach", headers={"Authorization": "jXIcrareVIEdvejaJkrhTrlbsiDc2D1BtUOSWoZw6sv5om8ggLuY6BWw"})
     print("Response received from Pexels API")
     response_json = json.loads(response.text)
     print("Response JSON loaded")
     video_urls = [v['video_files'][0]['link'] for v in response_json['videos']]
-    random.shuffle(video_urls)
-    print("Video URLs shuffled")
-
-    # Randomly pick 3 videos
-    video_urls = random.sample(video_urls, 3)
+    # Randomly pick 1 video
+    video_url = random.choice(video_urls)
 
     # Load audio file and detect bpm
     print("Loading audio file and detecting BPM...")
-    audio, sr = librosa.load("dreams.mp3.mp3", mono=True)
+    audio, sr = librosa.load("dreams.mp3", mono=True)
     audio_duration = librosa.get_duration(y=audio, sr=sr)
+    tempo = librosa.beat.tempo(y=audio, sr=sr)[0]
 
     # Convert audio data to mp3 file
     print("Converting audio data to mp3 file...")
@@ -42,21 +39,23 @@ def my_vid_gen_exec():
     subclips = []
 
     # Calculate the number of clips needed and the duration of each clip
-    num_clips = 100  # We will create 100 subclips
-    subclip_duration = audio_duration / num_clips  # Adjust the duration of each subclip so that they cover the entire audio
+    subclip_duration = 60 / tempo
+    num_clips = int(audio_duration / subclip_duration)
 
     print(f"Generating {num_clips} video clips of {subclip_duration} seconds each...")
 
+    try:
+        print(f"Downloading video file from {video_url}...")
+        urllib.request.urlretrieve(video_url, "temp.mp4")
+        print(f"Loading video file from {video_url}...")
+        video = VideoFileClip("temp.mp4")
+    except Exception as e:
+        print(f"Error: Unable to load video from {video_url}. Exiting...")
+        print(f"Exception: {str(e)}")
+        return
+
     for i in tqdm(range(num_clips), desc="Generating video clips"):
-        # Choose a video URL, looping back to the start if necessary
-        video_url = video_urls[i % len(video_urls)]
-        # Try to load video file
-        try:
-            video = VideoFileClip(video_url)
-        except OSError:
-            print(f"Error: Unable to load video from {video_url}")
-            continue
-        # Calculate start and end times for this subclip in the video
+        # Calculate start and end times for this subclip in the video, looping back to the start if necessary
         start_time = (i * subclip_duration) % video.duration
         end_time = start_time + subclip_duration
         if end_time > video.duration:
@@ -68,9 +67,14 @@ def my_vid_gen_exec():
     print("Concatenating subclips...")
     final_video = concatenate_videoclips(subclips)
 
-    print("Writing final video file with audio...")
+    print("Loading audio file...")
     final_audio = AudioFileClip("audio.mp3")
-    final_video.write_videofile("final_videoBPM.mp4", codec='libx264', audio=final_audio, audio_codec='aac')
+    
+    print("Finalizing video with audio...")
+    final_clip = final_video.set_audio(final_audio)
+
+    print("Writing final video file...")
+    final_clip.write_videofile("final_videoBPM.mp4", codec='libx264')
 
     print("Video generation completed!")
 
